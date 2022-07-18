2ada021f4b7b81b0cd3f36ff1f777c586ce87dee
nocarea_ <viktor.rellom@protonmail.com>
2022-07-18 13:36:11 +0200

    add user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_secure_password
   has_many :articles
   before_save { self.email = email.downcase }
   validates :username,  presence: true,
