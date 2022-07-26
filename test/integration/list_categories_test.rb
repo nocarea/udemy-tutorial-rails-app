655e212bcc75d95799f80f3b1f51919f6517ca4e
nocarea_ <viktor.rellom@protonmail.com>
2022-07-26 17:51:18 +0200

    added category integration test

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
-def setup 
-  @category = Category.new(name: "Sports")
-  @category2 = Category.new(name: "Travel")
-end
+  def setup 
+    @category = Category.create(name: "Sports")
+    @category2 = Category.create(name: "Travel")
+  end
+
+  test "should show categories listing" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
 end
